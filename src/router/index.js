386876df8fa67c5edcc8a928ca63b4ec6b2d31bb
Shin-3117 <tlswnd3127@gmail.com>
2026-08-12 14:26:21 +0900

    :recycle: WeatherMockup -> WeatherView View 명칭 명확하게 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WeatherMockup from '@/views/WeatherMockup.vue'
+import WeatherView from '@/views/WeatherView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: WeatherMockup,
+      component: WeatherView,
     },
     {
       path: '/test',

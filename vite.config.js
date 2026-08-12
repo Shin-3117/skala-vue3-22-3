a9a0e8488249428e46b005af13df8f9bdb37fe09
Shin-3117 <tlswnd3127@gmail.com>
2026-08-12 11:56:42 +0900

    add 자동 import

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), vueDevTools()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    vueDevTools(),
+    Components({
+      // 자동 검색할 컴포넌트 폴더 경로 (기본값: 'src/components')
+      dirs: ['src/components'],
+      // 검색할 파일 확장자
+      extensions: ['vue'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),

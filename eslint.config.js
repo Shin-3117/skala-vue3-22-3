afcf2cdc2c3d24b017fcfbe46b629761073aa22b
Shin-3117 <tlswnd3127@gmail.com>
2026-08-13 14:59:11 +0900

    :lipstick: eslint 설정 업데이트 - 주석 보강 및 설정 가독성 개선

--- eslint.config.js
+++ eslint.config.js
@@ -21,13 +21,14 @@ export default defineConfig([
     },
   },
 
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
+  js.configs.recommended, // js 기본 권장 사항 설정
+  ...pluginVue.configs['flat/essential'], //필수 문법 에러만
 
   {
     rules: {
       // 단일 단어 컴포넌트 이름 허용 (Badge.vue, Button.vue 등 UI 컴포넌트용)
       'vue/multi-word-component-names': 'off',
+      // 'no-console': 'off', // 콘솔 로그 출력을 무시
       // 'error' 대신 'warn'으로 낮추거나, 아예 안 보려면 'off' 처리
       'no-unused-vars': [
         'warn', // 혹은 'off'
@@ -41,5 +42,5 @@ export default defineConfig([
 
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
-  skipFormatting,
+  skipFormatting, // Formatting 관련 규칙을 무시 -> Prettier가 주로 담당
 ])
